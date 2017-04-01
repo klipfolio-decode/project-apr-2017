5f931de323e16b841d2c9e0a8291b8faf6fad1e5
bkzhang <brian.k.zhang@gmail.com>
2017-04-01 12:38:36 -0400

    fix rendering

--- www/dashboard.js
+++ www/dashboard.js
@@ -8,13 +8,14 @@ define([
         },
         renderDashboard: function() {
             // get the ids of all visualizations to render
+            $("#dashboard-container").html("");
             API.getSchemaList()
             .done(function(data) {
                 // get the schema for each viz
                 data.ids.forEach(function(id) {
                     API.getSchema(id)
                     .done(function(data) {
-                        // add the viz to the dashboard
+                        // add the viz to the dashboard 
                         $("#dashboard-container").append("<div>" + Viz.getVisualisation(data) + "<hr>");
                     });
                 });
