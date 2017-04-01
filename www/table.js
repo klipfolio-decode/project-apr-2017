bf50c22ee020c94681bef7f69c952c08c6f35e44
Christopher Poirier <cpoirier@klipfolio.com>
2017-04-01 11:54:49 -0400

    Dashboard render is working

--- www/table.js
+++ www/table.js
@@ -3,23 +3,21 @@ define([
 
 ], function() {
     return {
-        create: function(sample) {
-            //console.log(sample.style);
-            //console.log(sample.data[0].length);
-            var num_rows = sample.data[0].length;
-            var num_cols = sample.data.length;
-            var style = sample.style;
+        create: function(schema) {
+            var num_rows = schema.data[0].length;
+            var num_cols = schema.data.length;
+            var style = schema.style;
             var theader = "<table id='table1' style='margin: auto;' width = 80% ><tr>";
             var tbody = " ";
 
             for(var j = 0; j < num_cols; j++)
             {
             	theader += '<th style="background-color: ';
-            	theader += sample.style.headerColor;
+            	theader += schema.style.headerColor;
             	theader += '; color: '
-            	theader += sample.style.htextColor;
+            	theader += schema.style.htextColor;
             	theader += ' ;">'
-            	theader += sample.data[j][0];
+            	theader += schema.data[j][0];
             	theader += "</th>";
             }
             theader += '</tr></thead>';
@@ -30,17 +28,18 @@ define([
             	for(var j = 0; j < num_cols; j++)
             	{
             		tbody += '<td style="background-color: ';
-            		tbody += sample.style.fillColor;
+            		tbody += schema.style.fillColor;
             		tbody += '; color: '
-            		tbody += sample.style.textColor;
+            		tbody += schema.style.textColor;
             		tbody += ' ;">'
-            		tbody += sample.data[j][i];
+            		tbody += schema.data[j][i];
             		tbody += "</td>"
             	}
             	tbody += "</tr><br />";
             }
             var tfooter = "</table>";
-            document.getElementById('table').innerHTML = theader + tbody + tfooter;
+
+            return theader + tbody + tfooter;
         }
     };
 });
