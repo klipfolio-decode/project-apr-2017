044744aa8045118f6ad0b92a132ddc0a71791d13
bkzhang <brian.k.zhang@gmail.com>
2017-04-01 10:57:50 -0400

    working overlay

--- www/header.js
+++ www/header.js
@@ -9,10 +9,24 @@ define([
         $(".dropdown-content").toggle("show");
         // $("#dropbtn").toggle("change");
       });
+ 
+      function openAdd() {
+        document.getElementById("add").style.height = "100%";
+      }
+      function closeAdd() {
+        document.getElementById("add").style.height = "0%";
+      }
+
       $("#addbtn").click(function() {
-        console.log("Add button clicked!");
-        Page3.headers(1);
+        console.log("Add button clicked!"); 
+        openAdd();
+        Page3.headers(1); 
         //parseCSVFile();
+      });
+
+      $(".closebtn").click(function() {
+        $("#page-3").html(""); 
+        closeAdd();
       });
     }
   };
