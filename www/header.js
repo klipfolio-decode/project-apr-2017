fa1b0b7e759ea6752c8bc0a95eab50714882a590
melinapang <meng.pang@mail.mcgill.ca>
2017-03-31 20:29:50 -0400

    style added to app container

--- www/header.js
+++ www/header.js
@@ -1,17 +1,19 @@
 define([
-
+  'jquery'
 ], function() {
-    return {
-        init: function() {
-            $("#dropbtn").click(function(){
-                //alert( "Handler for .click() called." );
-                console.log("Menu button clicked!");
-                $(".dropdown-content").toggle("show");
-            });
-            $("#addbtn").click(function(){
-                console.log("Add button clicked!");
-                //parseCSVFile()
-            });
-        }
-    };
+  return {
+    init: function() {
+      $("#dropbtn").click(function() {
+        //alert( "Handler for .click() called." );
+        console.log("Menu button clicked!");
+        $(".dropdown-content").toggle("show");
+        // $("#dropbtn").toggle("change");
+
+      });
+      $("#addbtn").click(function() {
+        console.log("Menu button clicked!");
+      //parseCSVFile()
+      });
+    }
+  };
 });
