c24282248a1422e1f63b21a85a4551a06e933a74
bkzhang <brian.k.zhang@gmail.com>
2017-04-01 12:35:49 -0400

    preprepredemo

--- www/header.js
+++ www/header.js
@@ -1,6 +1,8 @@
 define([
-  "page3.js" 
-], function(Page3) {
+  "page3.js",
+  "api.js",
+  "dashboard.js"
+], function(Page3, API, Dashboard) {
   return {
     init: function() {         
       $("#dropbtn").click(function() {
@@ -9,7 +11,8 @@ define([
         $(".dropdown-content").toggle("show");
         // $("#dropbtn").toggle("change");
       });
- 
+
+      // page 3 overlay 
       function openAdd() {
         document.getElementById("add").style.height = "100%";
       }
@@ -27,6 +30,27 @@ define([
       $(".closebtn").click(function() {
         $("#page-3").html(""); 
         closeAdd();
+      });
+
+      $(".check").click(function() { 
+        var checked = [];
+        var elem = document.getElementsByClassName("page3check");
+        for(var i=0; elem[i]; i++){
+          if(elem[i].checked) {
+            checked.push(elem[i].value); 
+          }
+        }
+        console.log(checked);
+        if (checked.length === 0) {
+          alert("No headers");
+        } else {
+          API.createSchema(checked) 
+            .done(function(data) {
+              Dashboard.renderDashboard();
+              $("#page-3").html("");
+              closeAdd();
+            });
+        }
       });
     }
   };
